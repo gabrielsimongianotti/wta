67284349f1f79045abd4ea991282ca1a688c8f44
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2020-11-29 00:27:57 -0300

    add test to cache

--- backend/src/modules/gifts/services/ListGiftsService.spec.ts
+++ backend/src/modules/gifts/services/ListGiftsService.spec.ts
@@ -8,7 +8,7 @@ let fakeCacheProvider: FakeCacheProvider;
 let createGifts: CreateGiftsService;
 let listGifts: ListGiftsService;
 
-describe('istUser', () => {
+describe('listGifts', () => {
   beforeEach(() => {
     fakeGiftsRepository = new FakeGiftsRepository();
     fakeCacheProvider = new FakeCacheProvider();
@@ -20,13 +20,27 @@ describe('istUser', () => {
     listGifts = new ListGiftsService(fakeGiftsRepository, fakeCacheProvider);
   });
 
-  it('should be able to lisr a gifts', async () => {
+  it('should be able to list a gifts', async () => {
     const gift = await createGifts.execute({
       name: 'John Doe',
       description: 'g',
       level: 1,
       system: 'h',
     });
+
+    const list = await listGifts.execute();
+
+    await expect(list).toEqual([gift]);
+  });
+  it('should be able to list of cache', async () => {
+    const gift = await createGifts.execute({
+      name: 'John Doe',
+      description: 'g',
+      level: 1,
+      system: 'h',
+    });
+
+    await listGifts.execute();
 
     const list = await listGifts.execute();
 
