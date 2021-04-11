2005879f48f392dd83ad4c8b7edb6fd072362015
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-04-11 19:48:35 -0300

    create service to get data

---
+++ backend/src/modules/sheetSecondEdition/services/CreateSheetSecondEditionService.ts
@@ -0,0 +1,26 @@
+import { inject, injectable } from 'tsyringe';
+import AppError from '@shared/erros/AppError';
+import ISheetSecondEdition from '@modules/sheetSecondEdition/infra/typeorm/entities/SheetSecondEdition';
+import ISheetSecondEditionRepository from '../repositories/ISheetSecondEditionRepository';
+import ICreateSecondEditionpDTO from '@modules/sheetSecondEdition/dtos/ICreateSecondEditionpDTO';
+
+@injectable()
+class CreatSheetSecondEditionService {
+  constructor(
+    @inject('SheetSecondEditionRepository')
+    private sheetSecondEditionRepository: ISheetSecondEditionRepository,
+  ) { }
+
+  public async execute(data: ICreateSecondEditionpDTO): Promise<ISheetSecondEdition> {
+    const { user_id } = data;
+    const idValid = await this.sheetSecondEditionRepository.findByIdUser({ id: user_id })
+
+    if (!idValid) throw new AppError('Usuario invalido'+!idValid+idValid);
+
+    const sheetSecondEdition = await this.sheetSecondEditionRepository.create(data);
+
+    return sheetSecondEdition;
+  }
+}
+
+export default CreatSheetSecondEditionService;
