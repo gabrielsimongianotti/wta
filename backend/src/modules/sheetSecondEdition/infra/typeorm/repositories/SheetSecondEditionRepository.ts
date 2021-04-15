07b3a15f9e66133ea1ff533189efdc2ea3ea3bd7
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-04-15 00:59:37 -0300

    update file to have new methods of put

--- backend/src/modules/sheetSecondEdition/infra/typeorm/repositories/SheetSecondEditionRepository.ts
+++ backend/src/modules/sheetSecondEdition/infra/typeorm/repositories/SheetSecondEditionRepository.ts
@@ -2,6 +2,7 @@ import { getRepository, Repository, Raw } from 'typeorm';
 
 import ISheetSecondEditionRepository from '@modules/sheetSecondEdition/repositories/ISheetSecondEditionRepository';
 import ICreateSecondEditionpDTO from '@modules/sheetSecondEdition/dtos/ICreateSecondEditionpDTO';
+import IUpdataSecondEditionpDTO from '@modules/sheetSecondEdition/dtos/IUpdataSecondEditionpDTO';
 import SheetSecondEdition from '@modules/sheetSecondEdition/infra/typeorm/entities/SheetSecondEdition';
 import Users from '@modules/users/infra/typeorm/entities/Users';
 class SheetSecondEditionRepository implements ISheetSecondEditionRepository {
@@ -152,12 +153,158 @@ class SheetSecondEditionRepository implements ISheetSecondEditionRepository {
     return sheetSecondEdition;
   }
 
+  public async update({
+    id = "",
+    user_id = "",
+    name = "",
+    player = "",
+    augurio = "",
+    garou_name = "",
+    rage = 0,
+    spendingRage = 0,
+    gnosis = 0,
+    spendingGnosis = 0,
+    willpower = 0,
+    spendingWillpower = 0,
+    glory = 0,
+    spendingGlory = 0,
+    honor = 0,
+    spendingHonor = 0,
+    wisdom = 0,
+    spendingWisdom = 0,
+    rank = "Cliath",
+    experience = 0,
+    strength = 0,
+    dexterity = 0,
+    stamina = 0,
+    charisma = 0,
+    manipulation = 0,
+    appearance = 0,
+    perception = 0,
+    intelligence = 0,
+    wits = 0,
+    alertness = 0,
+    athletics = 0,
+    brawl = 0,
+    empathy = 0,
+    expression = 0,
+    intimidation = 0,
+    leadership = 0,
+    primal_urge = 0,
+    streetwise = 0,
+    subterfuge = 0,
+    animal_ken = 0,
+    crafts = 0,
+    drive = 0,
+    etiquette = 0,
+    firearms = 0,
+    larceny = 0,
+    melee = 0,
+    performance = 0,
+    stealth = 0,
+    survival = 0,
+    academics = 0,
+    computer = 0,
+    enigmas = 0,
+    investigation = 0,
+    law = 0,
+    medicine = 0,
+    occult = 0,
+    rituals = 0,
+    technology = 0,
+    science = 0,
+    gift_id = null,
+    flaw_id = null,
+    rite_id = null,
+    merit_id = null,
+    fetiches_id = null,
+    backgrounds_id = null
+  }: IUpdataSecondEditionpDTO): Promise<SheetSecondEdition> {
+    const edit = await this.ormRepository.save({
+      id,
+      user_id,
+      name,
+      player,
+      augurio,
+      garou_name,
+      rage,
+      spendingRage,
+      gnosis,
+      spendingGnosis,
+      willpower,
+      spendingWillpower,
+      glory,
+      spendingGlory,
+      honor,
+      spendingHonor,
+      wisdom,
+      spendingWisdom,
+      rank,
+      experience,
+      strength,
+      dexterity,
+      stamina,
+      charisma,
+      manipulation,
+      appearance,
+      perception,
+      intelligence,
+      wits,
+      alertness,
+      athletics,
+      brawl,
+      empathy,
+      expression,
+      intimidation,
+      leadership,
+      primal_urge,
+      streetwise,
+      subterfuge,
+      animal_ken,
+      crafts,
+      drive,
+      etiquette,
+      firearms,
+      larceny,
+      melee,
+      performance,
+      stealth,
+      survival,
+      academics,
+      computer,
+      enigmas,
+      investigation,
+      law,
+      medicine,
+      occult,
+      rituals,
+      technology,
+      science,
+      gift_id,
+      flaw_id,
+      rite_id,
+      merit_id,
+      fetiches_id,
+      backgrounds_id
+    });
+
+    return edit;
+  }
+
   public async findByIdUser({ id }: { id: string }): Promise<Users | undefined> {
     const user = await this.UsersRepository.findOne({
       where: { id },
     });
 
     return user;
+  }
+
+  public async findByIdSheet({ id }: { id: string; }): Promise<SheetSecondEdition | undefined> {
+    const Sheet = await this.ormRepository.findOne({
+      where: { id },
+    });
+
+    return Sheet;
   }
 }
 
