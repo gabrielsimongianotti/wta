1c202137d1a8e54d85885726fe393f552f0b992f
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-04-15 00:45:58 -0300

    add update and findByIdSheet in interface

--- backend/src/modules/sheetSecondEdition/repositories/ISheetSecondEditionRepository.ts
+++ backend/src/modules/sheetSecondEdition/repositories/ISheetSecondEditionRepository.ts
@@ -1,8 +1,11 @@
 import SheetSecondEdition from '../infra/typeorm/entities/SheetSecondEdition';
 import ICreateSheetSecondEditionDTO from '../dtos/ICreateSecondEditionpDTO';
+import IUpdateSecondEditionpDTO from '@modules/sheetSecondEdition/dtos/IUpdataSecondEditionpDTO';
 import Users from '@modules/users/infra/typeorm/entities/Users';
 
 export default interface ISheetSecondEditionRepository {
   create(data: ICreateSheetSecondEditionDTO): Promise<SheetSecondEdition>;
+  update(data: IUpdateSecondEditionpDTO): Promise<SheetSecondEdition>;
   findByIdUser({ id }: { id: string }): Promise<Users | undefined>
+  findByIdSheet({ id }: { id: string }): Promise<SheetSecondEdition | undefined>
 }
