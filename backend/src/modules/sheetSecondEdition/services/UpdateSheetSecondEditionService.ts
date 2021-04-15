e69bc67f0210ed66f971e734abf94ed660d13fee
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-04-15 00:41:29 -0300

    create service to do update

---
+++ backend/src/modules/sheetSecondEdition/services/UpdateSheetSecondEditionService.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from 'tsyringe';
+import AppError from '@shared/erros/AppError';
+import ISheetSecondEdition from '@modules/sheetSecondEdition/infra/typeorm/entities/SheetSecondEdition';
+import ISheetSecondEditionRepository from '../repositories/ISheetSecondEditionRepository';
+import IUpdataSecondEditionpDTO from '@modules/sheetSecondEdition/dtos/IUpdataSecondEditionpDTO';
+
+@injectable()
+class CreatSheetSecondEditionService {
+  constructor(
+    @inject('SheetSecondEditionRepository')
+    private sheetSecondEditionRepository: ISheetSecondEditionRepository,
+  ) { }
+
+  public async execute(data: IUpdataSecondEditionpDTO): Promise<ISheetSecondEdition> {
+    const { user_id, id } = data;
+
+    const idValidSheet = await this.sheetSecondEditionRepository.findByIdSheet({ id });
+   
+    if (!idValidSheet) throw new AppError('Id invalido');
+
+    const idUserValid = await this.sheetSecondEditionRepository.findByIdUser({ id: user_id });
+
+    if (!idUserValid) throw new AppError('Usuario invalido');
+  
+    const sheetSecondEdition = await this.sheetSecondEditionRepository.update(data);
+
+    return sheetSecondEdition;
+  }
+}
+
+export default CreatSheetSecondEditionService;
