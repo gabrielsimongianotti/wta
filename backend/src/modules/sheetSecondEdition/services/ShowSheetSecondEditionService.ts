15994236ac381fd249fed50682881beecffa0093
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-04-15 22:52:14 -0300

    create method to get

---
+++ backend/src/modules/sheetSecondEdition/services/ShowSheetSecondEditionService.ts
@@ -0,0 +1,24 @@
+import { inject, injectable } from 'tsyringe';
+import AppError from '@shared/erros/AppError';
+import ISheetSecondEdition from '@modules/sheetSecondEdition/infra/typeorm/entities/SheetSecondEdition';
+import ISheetSecondEditionRepository from '../repositories/ISheetSecondEditionRepository';
+import ICreateSecondEditionpDTO from '@modules/sheetSecondEdition/dtos/ICreateSecondEditionpDTO';
+
+@injectable()
+class CreatSheetSecondEditionService {
+  constructor(
+    @inject('SheetSecondEditionRepository')
+    private sheetSecondEditionRepository: ISheetSecondEditionRepository,
+  ) { }
+
+  public async execute(id: string): Promise<ISheetSecondEdition> {
+
+    const sheetSecondEdition = await this.sheetSecondEditionRepository.findByIdSheet({ id })
+    console.log(sheetSecondEdition)
+    if (!sheetSecondEdition) throw new AppError('id invalido');
+
+    return sheetSecondEdition;
+  }
+}
+
+export default CreatSheetSecondEditionService;
