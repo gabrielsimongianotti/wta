78673a9f298b704df1c94c01d0b90c0211eb5281
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-04-24 03:41:17 -0300

    remove import not used

--- backend/src/modules/sheetSecondEdition/services/ShowSheetSecondEditionService.ts
+++ backend/src/modules/sheetSecondEdition/services/ShowSheetSecondEditionService.ts
@@ -2,7 +2,6 @@ import { inject, injectable } from 'tsyringe';
 import AppError from '@shared/erros/AppError';
 import ISheetSecondEdition from '@modules/sheetSecondEdition/infra/typeorm/entities/SheetSecondEdition';
 import ISheetSecondEditionRepository from '../repositories/ISheetSecondEditionRepository';
-import ICreateSecondEditionpDTO from '@modules/sheetSecondEdition/dtos/ICreateSecondEditionpDTO';
 
 @injectable()
 class ShowSheetSecondEditionService {
@@ -14,7 +13,7 @@ class ShowSheetSecondEditionService {
   public async execute(id: string): Promise<ISheetSecondEdition> {
 
     const sheetSecondEdition = await this.sheetSecondEditionRepository.findByIdSheet({ id })
-    console.log(sheetSecondEdition)
+    
     if (!sheetSecondEdition) throw new AppError('id invalido');
 
     return sheetSecondEdition;
