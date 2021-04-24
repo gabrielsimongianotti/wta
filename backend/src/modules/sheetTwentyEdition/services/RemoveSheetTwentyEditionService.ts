efc002d7c85311c9095d929b35c98d81b5b49dff
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-04-24 16:06:06 -0300

    service of delete

---
+++ backend/src/modules/sheetTwentyEdition/services/RemoveSheetTwentyEditionService.ts
@@ -0,0 +1,22 @@
+import { inject, injectable } from 'tsyringe';
+import AppError from '@shared/erros/AppError';
+import ISheetTwentyEditionRepository from '../repositories/ISheetTwentyEditionRepository';
+
+@injectable()
+class RemoveSheetTwentyEditionService {
+  constructor(
+    @inject('SheetTwentyEditionRepository')
+    private sheetTwentyEditionRepository: ISheetTwentyEditionRepository,
+  ) { }
+
+  public async execute(id: string): Promise<void> {
+
+    const sheetTwentyEdition = await this.sheetTwentyEditionRepository.findByIdSheet({ id })
+    
+    if (!sheetTwentyEdition) throw new AppError('id invalido');
+
+    await this.sheetTwentyEditionRepository.delete({id});
+  }
+}
+
+export default RemoveSheetTwentyEditionService;
