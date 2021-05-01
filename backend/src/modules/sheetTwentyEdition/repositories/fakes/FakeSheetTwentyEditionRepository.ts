c96c52be28060e7e6edc092af867c3aa95aa8cb8
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-04-30 23:40:08 -0300

    implements new methods

--- backend/src/modules/sheetTwentyEdition/repositories/fakes/FakeSheetTwentyEditionRepository.ts
+++ backend/src/modules/sheetTwentyEdition/repositories/fakes/FakeSheetTwentyEditionRepository.ts
@@ -5,15 +5,19 @@ import ICreateTwentyEditionpDTO from '@modules/sheetTwentyEdition/dtos/ICreateTw
 import IUpdataTwentyEditionpDTO from '@modules/sheetTwentyEdition/dtos/IUpdataTwentyEditionpDTO';
 import SheetTwentyEdition from '@modules/sheetTwentyEdition/infra/typeorm/entities/SheetTwentyEdition';
 import User from '@modules/users/infra/typeorm/entities/Users';
+import Group from '@modules/group/infra/typeorm/entities/Group';
 import ICreateUsersDTO from '@modules/users/dtos/ICreateUsersDTO';
+import ICreateGroupDTO from '@modules/group/dtos/ICreateGroupDTO';
 
 class FakeSheetTwentyEditionRepository implements ISheetTwentyEditionRepository {
 
   private sheetTwentyEdition: SheetTwentyEdition[] = [];
   private users: User[] = [];
+  private groups: Group[] = [];
 
   public async create(data: ICreateTwentyEditionpDTO): Promise<SheetTwentyEdition> {
     const sheetTwentyEdition = new SheetTwentyEdition();
+    const groups = new Group()
 
     Object.assign(sheetTwentyEdition, { id: uuid(), ...data });
 
@@ -35,9 +39,21 @@ class FakeSheetTwentyEditionRepository implements ISheetTwentyEditionRepository 
     return sheetTwentyEdition;
   }
 
-  public async findByIdSheet({ id }: { id: string; }): Promise<SheetTwentyEdition | undefined> {
+  public async findByIdGroup(id: string): Promise<Group | undefined> {
+    const group = this.groups.find(group => group.id === id)
+
+    return group;
+  }
+
+  public async findByIdSheet(id: string): Promise<SheetTwentyEdition | undefined> {
     const sheet = this.sheetTwentyEdition.find(sheetTwentyEdition => sheetTwentyEdition.id === id)
- 
+
+    return sheet
+  }
+
+  public async findByIdGroupAndIdUser({ group_id, user_id }: { user_id: string; group_id: string }): Promise<SheetTwentyEdition | undefined> {
+    const sheet = this.sheetTwentyEdition.find(sheetTwentyEdition => sheetTwentyEdition.user_id === user_id && sheetTwentyEdition.group_id === group_id)
+
     return sheet
   }
 
@@ -51,13 +67,22 @@ class FakeSheetTwentyEditionRepository implements ISheetTwentyEditionRepository 
     return user
   }
 
-  public async findByIdUser({ id }: { id: string; }): Promise<User | undefined> {
+  public async addGroup(userData: ICreateGroupDTO): Promise<Group> {
+    const group = new Group();
+
+    Object.assign(group, { id: uuid() }, userData);
+
+    await this.groups.push(group);
+
+    return group
+  }
+  public async findByIdUser(id: string): Promise<User | undefined> {
     const user = this.users.find(user => user.id === id)
- 
+
     return user
   }
 
-  public async  delete({ id }: { id: string; }): Promise<void> {
+  public async delete({ id }: { id: string; }): Promise<void> {
     await this.sheetTwentyEdition.splice(
       this.sheetTwentyEdition.findIndex(sheet => sheet.id === id),
       1,
