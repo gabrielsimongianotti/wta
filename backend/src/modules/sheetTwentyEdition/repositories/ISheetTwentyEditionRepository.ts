91325d659d7f19eeca8b5b8300c803a6b7e844c3
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2021-04-30 23:41:50 -0300

    alter findByIdSheet findByIdUserand, add findByIdGroupAndIdUser and findByIdGroup

--- backend/src/modules/sheetTwentyEdition/repositories/ISheetTwentyEditionRepository.ts
+++ backend/src/modules/sheetTwentyEdition/repositories/ISheetTwentyEditionRepository.ts
@@ -2,11 +2,14 @@ import SheetTwentyEdition from '../infra/typeorm/entities/SheetTwentyEdition';
 import ICreateTwentyEditionpDTO from '../dtos/ICreateTwentyEditionpDTO';
 import IUpdataTwentyEditionpDTO from '@modules/sheetTwentyEdition/dtos/IUpdataTwentyEditionpDTO';
 import Users from '@modules/users/infra/typeorm/entities/Users';
+import Group from '@modules/group/infra/typeorm/entities/Group';
 
 export default interface ISheetTwentyEditionRepository {
   create(data: ICreateTwentyEditionpDTO): Promise<SheetTwentyEdition>;
   update(data: IUpdataTwentyEditionpDTO): Promise<SheetTwentyEdition>;
-  findByIdUser({ id }: { id: string }): Promise<Users | undefined>
-  findByIdSheet({ id }: { id: string }): Promise<SheetTwentyEdition | undefined>
+  findByIdUser(id: string): Promise<Users | undefined>
+  findByIdGroup(id: string): Promise<Group | undefined>
+  findByIdGroupAndIdUser({ user_id, group_id }: { user_id: string, group_id: string; }): Promise<SheetTwentyEdition | undefined>
+  findByIdSheet(id: string): Promise<SheetTwentyEdition | undefined>
   delete({ id }: { id: string }): Promise<void>
 }
